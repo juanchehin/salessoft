6ea9dd87b43c5c97617206c74c8ba4f5e906e3ed
chehin <chehinjuan@gmail.com>
2023-01-17 11:38:31 -0300

    first commit

---
+++ CapaConexion/Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+
+namespace CapaConexion
+{
+    public class Conexion
+    {
+        private string cadena = "Server=localhost\\SQLEXPRESS;Database=la_fortuna;Trusted_Connection=True;";
+        private SqlConnection cn;
+
+        public SqlConnection conectar_db()
+        {
+            cn = new SqlConnection(cadena);
+
+            if (cn.State == ConnectionState.Open)
+            {
+
+                cn.Close();
+            }
+            else
+            {
+                cn.Open();
+            }
+
+            return cn;
+
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# SalesSoft
+
+https://drive.google.com/drive/folders/17i2msoORzXKr3s7_VeEDb0EHRWm9rbs6?usp=share_link
